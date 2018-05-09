2bf01fcea9c47019d965dcfb72502f0ad3db14b8
Isaac Ndung'u <inwainaina93@gmail.com>
2018-05-09 16:16:45 +0300

    [Chore] Refactor Script.

--- deploy-cp.sh
+++ deploy-cp.sh
@@ -81,10 +81,10 @@ function setup_nginx() {
                     # reverse proxy and serve the app
                     # running on the localhost:8000
                     proxy_pass http://127.0.0.1:8000/;
-                    proxy_set_header HOST $host;
-                    proxy_set_header X-Forwarded-Proto $scheme;
-                    proxy_set_header X-Real-IP $remote_addr;
-                    proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+                    proxy_set_header HOST \$host;
+                    proxy_set_header X-Forwarded-Proto \$scheme;
+                    proxy_set_header X-Real-IP \$remote_addr;
+                    proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
             }
     }'
 
